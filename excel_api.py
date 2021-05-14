4ca0933093ac96d46189e8f9093b0ced9081dcc7
akshay naik <akshaynaik797@gmail.com>
2021-05-14 17:29:55 +0530

    0.0.40.1

--- excel_api.py
+++ excel_api.py
@@ -16,27 +16,59 @@ col_mapping = {
     "bajaj": [['CLAIM NO', ["ALNO", "ClaimNo"]], ['PATIENT NAME', ["PatientName"]], ['DOA', ["DateofAdmission"]],
               ['DOD', ["DateofDischarge"]], ['GROSS AMOUNT', ["BilledAmount", "SettledAmount"]],
               ['TDS AMOUNT', ["TDS"]], ['NET AMOUNT', ["NetPayable"]], ['UTR NO', ["UTRNo"]], ["bajaj", "InsurerID"],
-              ["bajaj", "TPAID"], ['TRANSFER DATE', ["Transactiondate"]]]
-}
+              ["bajaj", "TPAID"], ['TRANSFER DATE', ["Transactiondate"]]],
 
+    "fhpl": [['Claim Id', ["ClaimNo"]], ['Uhid No', ["MemberID"]], ['PatientName', ["PatientName"]],
+             ['PA No', ["ALNO"]], ['Insurer Name', ["InsurerID"]], ['Claimed Amt', ["BilledAmount"]],
+             ['Settled Amt', ["SettledAmount", "NetPayable"]], ['Cheque/NEFT No', ["UTRNo"]],
+             ['Cheque/NEFT Date', ["Transactiondate"]], ['DOA', ["DateofAdmission"]], ['DOD', ["DateofDischarge"]],
+             ["fhpl", "TPAID"]],
+
+    "genins": [['ClaimNo', ["ALNO", "ClaimNo"]], ['CardNo', ['MemberID']], ['PatientName', ['PatientName']],
+               ['PolicyNo', ['POLICYNO']], ['Hospfrom', ['DateofAdmission']], ['Hospto', ['DateofDischarge']],
+               ['Disease', ['Diagnosis']], ['Amount Auth', ['SettledAmount']], ['Amount Claimed', ['BilledAmount']],
+               ['Amount Paid', ['NetPayable']], ['Chequeno', ['UTRNo']], ['ChequeDate', ['Transactiondate']],
+               ['tds', ['TDS']], ["genins", "InsurerID"], ["genins", "TPAID"]],
+
+    "icici": [['Policy Number', ['POLICYNO']], ['Name Of The Insured', ['PatientName']],
+              ['Card Number/UHID', ['MemberID']], ['Co-Payment Value', ['Copay']], ['AL-Number', ['ALNO']],
+              ['Claim-Amount Claimed', ['BilledAmount']], ['Claim-Sanctioned Amount', ['SettledAmount']],
+              ['Claim-Disallowed Amount', ['Discount']], ['Claim-Cheque Date', ['Transactiondate']],
+              ['Claim-Cheque Number', ['UTRNo']], ['Claim-TDS Amt', ['TDS']], ['Claim-Transferred Amt', ['NetPayable']],
+              ['Claim Number', ['ClaimNo']], ["icici", "TPAID"]],
+
+    "mediassist": [['PatientName', ['PatientName']], ['InsuranceCompany', ['InsurerID']],
+                   ['ClaimId', ["ALNO", "ClaimNo"]], ['DOA', ['DateofAdmission']], ['DOD', ['DateofDischarge']],
+                   ['ClaimAmount', ['BilledAmount']], ['ClaimApprovedAmt', ['SettledAmount']],
+                   ['ClaimNetPayAmount', ['NetPayable']], ['SettlementDate', ['Transactiondate']],
+                   ['ClaimChequeNumber', ['UTRNo']], ['Tds', ['TDS']], ["mediassist", "TPAID"]],
+
+    "paramount": [['PHM', ['MemberID']], ['NAME_OF_INSURANCE_CO', ['InsurerID']], ['FIR', ["ALNO", "ClaimNo"]],
+                  ['NAME', ['PatientName']], ['HOSPITAL_BILL_AMT', ['BilledAmount']], ['DISC_AMT', ['Discount']],
+                  ['TDS_AMT', ['TDS']], ['AMOUNT_PAID', ['NetPayable']], ['DT_OF_PAYMENT', ['Transactiondate']],
+                  ['DT_OF_ADMISSION', ['DateofAdmission']], ['DT_OF_DISCHARGE', ['DateofDischarge']],
+                  ['UTR_NO', ['UTRNo']], ["paramount", "TPAID"]]
+}
 
 def get_data_dict(col_mapping, row, tpa_id):
     mapping = col_mapping[tpa_id]
-    tmp = {}
+    data_dict = {}
     for i, j in mapping:
         if isinstance(j, list):
             for k in j:
-                tmp[k] = row[i]
+                data_dict[k] = row[i].strip()
         if isinstance(j, str):
-            tmp[j] = i
-    return tmp
+            data_dict[j] = i.strip()
+    if tpa_id == 'fhpl':
+        data_dict['ClaimNo'] = data_dict['ClaimNo'].split('/')[0]
+    return data_dict
 
 
 def get_data(excel_file):
-    _, ext = os.path.splitext(path)
+    _, ext = os.path.splitext(excel_file)
     data = []
     if ext == '.xlsx':
-        wb_obj = openpyxl.load_workbook(path)
+        wb_obj = openpyxl.load_workbook(excel_file)
         sheet_obj = wb_obj.active
         table = []
         for cnt, i in enumerate(sheet_obj.rows):
@@ -52,13 +84,13 @@ def get_data(excel_file):
             data.append(tmp)
 
     if ext == '.xls':
-        wb_obj = xlrd.open_workbook(path)
+        wb_obj = xlrd.open_workbook(excel_file)
         sheet_obj = wb_obj.sheet_by_index(0)
         table = []
         for row in range(sheet_obj.nrows):
             tmp = []
             for cell in sheet_obj.row(row):
-                tmp.append(cell.value)
+                tmp.append(str(cell.value))
             table.append(tmp)
         fields = table[0]
         for row in table[1:]:
@@ -77,6 +109,7 @@ def date_formatting(date):
     # 12 Feb 2021
     # 12 Feb 21
     formats = ['%d/%m/%Y', '%d-%m-%Y', '%d-%b-%Y', '%d-%b-%y', '%d %b %Y', '%d %b %y']
+    date = date.replace('00:00:00', '')
     date = date.strip()
     for i in formats:
         try:
@@ -128,8 +161,8 @@ def ins_upd_data(datadict):
 
 
 if __name__ == '__main__':
-    path = "/home/akshay/Downloads/Excel/Bajaj Paid Claim Details.xlsx"
+    path = "/home/akshay/Downloads/Excel/Paramount_01April2020-30April2021PaidClaims.xlsx"
     data = get_data(path)
-    data_dict = get_data_dict(col_mapping, data[0], "bajaj")
+    data_dict = get_data_dict(col_mapping, data[0], "paramount")
     a = ins_upd_data(data_dict)
 pass
